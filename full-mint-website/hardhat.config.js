0a48a5a6140c5b966914dc4bcf882f1eb360d8ae
Rising Star <star-atnight@outlook.com>
2023-03-10 13:48:04 +0800

    Deploy and verify the smart contract on Goerli testnet

--- full-mint-website/hardhat.config.js
+++ full-mint-website/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/7ce04ca962b6484b8513f27d9f53537c",
+      accounts: ["2c6d590af172ce5606e3a0f5718a83a06f595fc92d553417cb745a524c4f5dd6"],
+    },
+  },
+  etherscan: {
+    apiKey: "CPF3UWIGYTD8WAQD7UXUV5ZRMRAXYUECIB"
+  }
 };
--- full-mint-website/scripts/deployRoboPunksNFT.js
+++ full-mint-website/scripts/deployRoboPunksNFT.js
@@ -4,23 +4,16 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const RoboPunksNFT = await hre.ethers.getContractFactory("RoboPunksNFT");
+  const roboPunksNFT = await RoboPunksNFT.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await roboPunksNFT.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `RoboPunksNFT deployed to: ${roboPunksNFT.address}`
   );
 }
 
